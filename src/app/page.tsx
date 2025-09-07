bb26717bfa2f9abc772f6c5fd2cecb10c8ca1f69
cRazyeye19 <jlesterpansoy@gmail.com>
2025-09-07 13:29:05 +0800

    feat(auth) implement Supabase client helpers for SSR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-
+import { Typography } from "@mui/material";
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Typography variant="h1">Hello World</Typography>
     </>
   );
 }
