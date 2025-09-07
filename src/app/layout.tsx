1dc496025785b743eb1f977461362a7649bf4e8e
cRazyeye19 <jlesterpansoy@gmail.com>
2025-09-07 12:11:40 +0800

    feat(ui) add material-ui for styling and theming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,27 @@
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { Roboto } from "next/font/google";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@/theme";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={roboto.variable}>
+      <AppRouterCacheProvider>
+        <ThemeProvider theme={theme}>
+          <body>{children}</body>
+        </ThemeProvider>
+      </AppRouterCacheProvider>
     </html>
   );
 }
